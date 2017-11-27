c5e596cdfd4099a0e6b4e5371fabd27c9bc0f12c
shashasun <shashasun@pptv.com>
2017-11-27 19:01:30 +0800

    learn

--- newexerise/flie/server.js
+++ newexerise/flie/server.js
@@ -12,4 +12,17 @@ server.post('/upload', function (req, res){
   console.log(req.files);
   res.send('ok');
 });
+
+/*
+server.get('/1.txt', function (req, res){
+  res.setHeader('Content-Disposition', 'attachment; filename=1.txt');
+  res.sendFile('d:\\node\\www\\1.txt');
+});
+*/
+//浏览器和服务器之间所有的通信过程是由头来控制的
+// server.get('/1.txt',function(req,res){
+//   res.setHeader('Content-Disposition','attachment;filename=1.txt');
+//   res.sendFile('E:\\ssslearn\\learn\\newexerise\\flie\\www\\1.txt');
+// })
+
 server.use(express.static('./www/'))
